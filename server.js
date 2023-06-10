bc1dc283387def92a4d68169497a5cf9bcadc493
Aayu-shh <aayush28.official@gmail.com>
2023-06-11 02:21:14 +0530

    Edit (UPDATE) Functionality added

--- server.js
+++ server.js
@@ -10,33 +10,54 @@ const app = express();
 dotenv.config();
 
 app.use(cors());
-app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.redirect('index.html');
 });
 
-app.post('/users',(req,res,next)=>{
+app.post('/users', (req, res) => {
     var name = req.body.name;
     var email = req.body.email;
     var phone = req.body.phone;
-    db.execute("INSERT INTO users(Name,Phone_Number,Email) values(?,?,?)",[name,phone,email])
-    .then(() => res.redirect("/users") )
-    .catch(err=>console.log(err));    
+    db.execute("INSERT INTO users(Name,Phone_Number,Email) values(?,?,?)", [name, phone, email])
+        .then(() => res.redirect("/users"))
+        .catch(err => console.log(err));
 })
 
-app.get('/users',(req,res,next)=>{
+app.get('/users', (req, res) => {
     db.execute("SELECT * FROM users")
-        .then(result => res.render('users',{
-            results : result[0]
+        .then(result => res.render('users', {
+            results: result[0]
         }));
 })
 
-app.get('/delete/:uid', (req, res, next) => {
+app.get('/edit/:uid', (req, res) => {
+    let uid = req.params.uid;
+    db.execute('SELECT * FROM users WHERE id = ?', [uid])
+        .then(result => res.render('edit', { result: result[0][0] }))
+        .catch(err => console.log(err));
+})
+
+app.post('/edit/:uid', (req, res) => {
+    let uid = req.params.uid;
+    var name = req.body.name;
+    var email = req.body.email;
+    var phone = req.body.phone;
+    db.execute('DELETE FROM users WHERE id = ?', [uid])
+        .then(() => {
+            db.execute("INSERT INTO users(Name,Phone_Number,Email) values(?,?,?)", [name, phone, email])
+                .then(() => res.redirect("/users"))
+                .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+})
+
+app.get('/delete/:uid', (req, res) => {
     let uid = req.params.uid;
     db.execute('DELETE FROM users WHERE id = ?', [uid])
         .then(() => res.redirect('/users'))
@@ -44,6 +65,6 @@ app.get('/delete/:uid', (req, res, next) => {
 })
 
 app.listen(process.env.PORT, (err) => {
-    if(err) console.log(err);
-    else console.log('listening to port '+ process.env.PORT);
+    if (err) console.log(err);
+    else console.log('listening to port ' + process.env.PORT);
 })
